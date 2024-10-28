f8110b291ff3c89f97fe1c69737604682ffb4051
Chirag <chiragaggarwal51@yahoo.com>
2024-10-29 00:07:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Children } from "react";
+
+const CustomSwitch = ({ children, value }) => {
+  let cases = [];
+  let defaults = [];
+  Children.forEach(children, (e) => {
+    if (e.type.name === "CustomCase") {
+      if (typeof e.props.value === "function" && e.props.value(value)) {
+        cases.push(e);
+
+      }
+        else if (e.props.value === value) {
+          cases.push(e);
+        }
+      }
+     else if(e.type.name === "DefaultCase") {
+      defaults.push(e);
+    }
+  });
+
+  if (cases.length > 0) {
+    return cases;
+  } else {
+    return defaults;
+  }
+};
+
+const CustomCase = ({ children }) => {
+  return <>{children}</>;
+};
+
+const DefaultCase = ({ children }) => {
+  return <>{children}</>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <CustomSwitch value={4}>
+        <CustomCase
+          value={(e) => {
+            return e > 10;
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>Hello 10</div>
+        </CustomCase>
+        <CustomCase value={20}>Hello 20</CustomCase>
+        <CustomCase value={30}>Hello 30</CustomCase>
+        <CustomCase value={10}>Hello 10</CustomCase>
+        <DefaultCase >Hello 40</DefaultCase>
+      </CustomSwitch>
+    </>
   );
 }
 
